56c844bdfc8718287c6be062439321786d0b8706
naingoted <boshan55@gmail.com>
2016-03-01 06:20:05 +0800

    1 st  March
    education, index and half hospital details.

---
+++ js/mspiral_core.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function() {
+
+  jQuery('.jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
+
+        // This shows 1 item at a time.
+        // Divide `width` to the number of items you want to display,
+        // eg. `width = width / 3` to display 3 items at a time.
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    .jcarousel({
+      animation: 'fast',
+      transitions: true
+  });
+
+  jQuery('.jcarousel-prev').jcarouselControl({
+    target: '-=1'
+  });
+
+  jQuery('.jcarousel-next').jcarouselControl({
+    target: '+=1'
+  });
+/*  jQuery('.bxslider').bxSlider({
+
+  });*/
+    jQuery("#head").load("header.html"); 
+    jQuery("#foot").load("footer.html");
+});
+
+$(function(){
+  
+  $("#head").load("header.html");
+  $("#foot").load("footer.html");
+});
